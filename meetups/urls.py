a917c2a5304e0e710944dffccb951d91b2c70b05
tsytsyk.svitlana <tsytsyk.svetlana@gmail.com>
2023-06-22 14:33:28 +0300

    detail page added

--- meetups/urls.py
+++ meetups/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('meetups/', views.index)
+    path('meetups/', views.index),
+    path('meetups/<slug:meetup_slug>', views.metup_details)
 ]
--- meetups/views.py
+++ meetups/views.py
@@ -5,10 +5,22 @@ from django.http import HttpResponse
 
 def index(request):
     meetups=[
-        {"title" : "A First Meetup", 'location': 'New York', 'slug': 'a-first-setup'},
-        {"title" : "A Second Meetup", 'location': 'Paris', 'slug': 'a-second-setup'},
+        {"title" : "A First Meetup", 
+         'location': 'New York', 'slug': 
+         'a-first-setup'},
+        {"title" : "A Second Meetup", 
+         'location': 'Paris', 
+         'slug': 'a-second-setup'},
     ]
     return render(request, 'meetups/index.html', {
         'show_meetups' : True,
         'meetups' : meetups
+    })
+
+def metup_details(request, meetup_slug):
+    selected_meetup={"title" : "A First Meetup",
+                     'description': 'This is the first meetup!'}
+    return render(request, 'meetups/meetup-details.html', {
+        'meetup_title': selected_meetup['title'],
+        'meetup_description' : selected_meetup['description']
     })
