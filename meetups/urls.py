d1f0730dca3a1d7afb369a00fc0ab7d9df34cb07
tsytsyk.svitlana <tsytsyk.svetlana@gmail.com>
2023-06-22 23:40:06 +0300

    dynamic urls added

--- meetups/urls.py
+++ meetups/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('meetups/', views.index),
-    path('meetups/<slug:meetup_slug>', views.metup_details)
+    path('meetups/', views.index, name='all-meetups'),
+    path('meetups/<slug:meetup_slug>', views.metup_details, name="meetup-detail")
 ]
