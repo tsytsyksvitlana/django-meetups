47f527e06262de675a349db45ae8b00d33ff2bdb
tsytsyk.svitlana <tsytsyk.svetlana@gmail.com>
2023-07-04 22:36:39 +0300

    meetups changed

--- meetups/models.py
+++ meetups/models.py
@@ -18,6 +18,8 @@ class Participant(models.Model):
 
 class Meetup(models.Model):
     title=models.CharField(max_length=200)
+    organizer_email = models.EmailField()
+    date= models.DateField()
     slug=models.SlugField(unique=True)
     description=models.TextField()
     image = models.ImageField(upload_to='images')
--- meetups/views.py
+++ meetups/views.py
@@ -15,8 +15,7 @@ def metup_details(request, meetup_slug):
         selected_meetup= Meetup.objects.get(slug=meetup_slug)
         return render(request, 'meetups/meetup-details.html', {
             'meetup_found' : True,
-            'meetup_title': selected_meetup.title,
-            'meetup_description' : selected_meetup.description
+            'meetup': selected_meetup
         })
     except Exception as exc:
         return render(request, 'meetups/meetup-details.html', {
