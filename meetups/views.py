aa8750b0fea5a158252932028fd0ad885fd40a9b
tsytsyk.svitlana <tsytsyk.svetlana@gmail.com>
2023-07-05 21:39:34 +0300

    handling form submission

--- meetups/views.py
+++ meetups/views.py
@@ -14,12 +14,18 @@ def index(request):
 def metup_details(request, meetup_slug):
     try:
         selected_meetup= Meetup.objects.get(slug=meetup_slug)
-        registration_form = RegistrationForm()
+        if request.method == 'GET':
+            registration_form = RegistrationForm()
+        else:
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                participant =  registration_form.save()
+                selected_meetup.participants.add(participant)
         return render(request, 'meetups/meetup-details.html', {
-            'meetup_found' : True,
-            'meetup': selected_meetup,
-            'form': registration_form
-        })
+                'meetup_found' : True,
+                'meetup': selected_meetup,
+                'form': registration_form
+            })
     except Exception as exc:
         return render(request, 'meetups/meetup-details.html', {
             'meetup_found' : False
